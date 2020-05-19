ce2289a8cf8bbbf7bbaa182ddc18586f6949b8f1
mail2 <mail2@DESKTOP-0RRPI0P>
2020-05-19 18:28:46 +0530

    Code refactoring

---
+++ employee-service/src/main/java/org/kashif/trainings/employeeservice/app/EmployeeServiceApplication.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package org.kashif.trainings.employeeservice.app;
+
+
+import java.util.Collection;
+import java.util.logging.Logger;
+
+import org.kashif.trainings.employeeservice.model.Employee;
+import org.kashif.trainings.employeeservice.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author kashif
+ *
+ */
+
+@SpringBootApplication
+@RestController
+@ComponentScan(basePackages = "org.kashif.trainings.employeeservice")
+public class EmployeeServiceApplication implements CommandLineRunner {
+	
+	private static final Logger logger = Logger.getLogger("EmployeeServiceApplication");
+	
+	@Autowired
+	private EmployeeService employeeService;
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeServiceApplication.class, args);
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("EmployeeServiceApplication Started");
+		
+	}
+
+
+	@RequestMapping("/")
+	public Collection<Employee> getAllEmployees(){
+		return employeeService.getAllEmployees();
+	}
+
+	@RequestMapping("/employee/{employeeId}")
+	public Employee getEmployee(@PathVariable long employeeId) {
+		return employeeService.getEmployee(employeeId);
+	}
+
+}
---
+++ employee-service/src/main/java/org/kashif/trainings/employeeservice/model/Employee.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package org.kashif.trainings.employeeservice.model;
+
+/**
+ * @author kashif
+ *
+ */
+public class Employee {
+	
+	public Employee(long id, String firsName, String lastName, String email) {
+		this.employeeId = id;
+		this.firstName = firsName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the employeeId
+	 */
+	public long getEmployeeId() {
+		return employeeId;
+	}
+	/**
+	 * @param employeeId the employeeId to set
+	 */
+	public void setEmployeeId(long employeeId) {
+		this.employeeId = employeeId;
+	}
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private long employeeId;
+}
